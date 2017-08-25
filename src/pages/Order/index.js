5658712ee7b5873d0189887644bce350fde044ef
Шамиль Махмутов <SMMahmutov@domclick.ru>
2017-08-26 01:54:45 +0300

    map

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -6,58 +6,64 @@ import callsActions from '../../actions/callsList';
 import './styles.scss';
 
 @connect(
-    state => ({
-        // callsList: state.callsList
-    }),
-    dispatch => ({
-        // actions: bindActionCreators({...callsActions}, dispatch)
-    })
+  state => ({
+    // callsList: state.callsList
+  }),
+  dispatch => ({
+    // actions: bindActionCreators({...callsActions}, dispatch)
+  })
 )
 export default class Order extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            people: []
-        }
-
+  constructor() {
+    super();
+    this.state = {
+      people: []
     }
+  }
 
-    render() {
+  render() {
 
-        return (
-            <div>
-                <div>
-                    <h1>Закупка Табака для Кальяна</h1>
-                </div>
-                <div>
-                    <form className="form-inline">
-                        <div className="form-group">
-                            <label htmlFor="peopleCountInput">Введите кол-во человек </label>
-                            <input
-                                type="number"
-                                id="peopleCountInput"
-                                className="form-control"
-                                onChange={e => {
-                                    this.setState({people: new Array(e.target.value || 0)})
-                                }}
-                            />
-                        </div>
-                    </form>
-                </div>
-                <div>
-                    {
-                        this.state.people.map((el, key) => {
-                            console.log(el, key);  s
-                            return (
-                                <div>
-                                    <input type="text" key={key} />
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                <div>Количество людей - {this.state.people.length}</div>
+    return (
+      <div>
+        <div>
+          <h1>Закупка Табака для Кальяна</h1>
+        </div>
+        <div>
+          <form className="form-inline">
+            <div className="form-group">
+              <label htmlFor="peopleCountInput">Введите кол-во человек </label>
+              <input
+                type="number"
+                id="peopleCountInput"
+                className="form-control"
+                value={this.state.value}
+                onChange={e => {
+                  const arr = [];
+
+                  let count = +e.target.value || 0;
+                  if (count > 20) {
+                    count = 20;
+                  }
+
+                  for (let i = 0; i < count; i++) {
+                    arr.push(<div><input key={i} maxLength={20} /></div>);
+                  }
+
+                  this.setState({
+                    people: arr
+                  })
+                }}
+              />
             </div>
-        )
-    };
+          </form>
+        </div>
+
+
+        <div>
+          {this.state.people}
+        </div>
+        <div>Количество людей - {this.state.people.length}</div>
+      </div>
+    )
+  };
 }
